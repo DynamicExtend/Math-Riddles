e2fbd4530aa30d170c4d345f78ca5eda7b36690d
Jannik_Wiest <jannik.wiest@gmail.com>
2023-08-21 14:37:36 +0200

    Created loop for prisiner creation

---
+++ OneHundredHats/OneHundredHats.py
@@ -0,0 +1,12 @@
+import random
+import math
+
+#OneHundredHats on a scale of 10 Prisiners
+
+class Prisiner:
+    def __init__(self, hatColor):
+        self.hatColor = hatColor
+
+for i in range(100):
+    name = "prisiner" + str(i)
+    print(name)
